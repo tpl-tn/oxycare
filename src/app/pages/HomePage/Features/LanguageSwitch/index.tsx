79b79a4863bf3f54afc0efd4cd055ef1b024b583
ines Chebil <ines.chebil26@gmail.com>
2021-04-28 22:13:13 +0200

    fix#16

--- src/app/pages/HomePage/Features/LanguageSwitch/index.tsx
+++ src/app/pages/HomePage/Features/LanguageSwitch/index.tsx
@@ -81,6 +81,159 @@ export function LanguageSwitch() {
           value="7"
           isSelected={gov === 7}
         />
+        <Radio
+          id="8"
+          label="جندوبة"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="8"
+          isSelected={gov === 8}
+        />
+        <Radio
+          id="9"
+          label="زغوان"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="9"
+          isSelected={gov === 9}
+        />
+        <Radio
+          id="10"
+          label="سليانة"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="10"
+          isSelected={gov === 10}
+        />
+        <Radio
+          id="11"
+          label="سوسة"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="11"
+          isSelected={gov === 11}
+        />
+        <Radio
+          id="12"
+          label="سيدي بوزيد"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="12"
+          isSelected={gov === 12}
+        />
+        <Radio
+          id="13"
+          label=" صفاقس"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="13"
+          isSelected={gov === 13}
+        />
+        <Radio
+          id="14"
+          label=" قابس"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="14"
+          isSelected={gov === 14}
+        />
+        <Radio
+          id="15"
+          label=" القصرين"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="15"
+          isSelected={gov === 15}
+        />
+        <Radio
+          id="16"
+          label="قفصة"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="16"
+          isSelected={gov === 16}
+        />
+        <Radio
+          id="17"
+          label="القيروان"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="17"
+          isSelected={gov === 17}
+        />
+        <Radio
+          id="18"
+          label="الكاف"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="18"
+          isSelected={gov === 18}
+        />
+        <Radio
+          id="19"
+          label="مدنين"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="19"
+          isSelected={gov === 19}
+        />
+        <Radio
+          id="20"
+          label="مدنين"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="20"
+          isSelected={gov === 20}
+        />
+        <Radio
+          id="21"
+          label="المنستير"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="21"
+          isSelected={gov === 21}
+        />
+        <Radio
+          id="22"
+          label="منوبة"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="22"
+          isSelected={gov === 22}
+        />
+        <Radio
+          id="23"
+          label="المهدية"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="23"
+          isSelected={gov === 23}
+        />
+        <Radio
+          id="24"
+          label="نابل"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="24"
+          isSelected={gov === 24}
+        />
       </Languages>
     </Wrapper>
   );
@@ -95,8 +248,9 @@ const Wrapper = styled.div`
 `;
 const Languages = styled.div`
   display: flex;
-
+  flex-wrap: wrap;
   .radio {
     margin-right: 1.5rem;
+    width: 20%;
   }
 `;
