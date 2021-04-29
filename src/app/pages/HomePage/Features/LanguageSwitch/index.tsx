e5c2eeb728de286984eccbe2d330ebbc74b80d53
KMx404 <mohamedelfaleh5@gmail.com>
2021-04-29 17:56:08 +0100

    edits

--- src/app/pages/HomePage/Features/LanguageSwitch/index.tsx
+++ src/app/pages/HomePage/Features/LanguageSwitch/index.tsx
@@ -1,19 +1,23 @@
-import * as React from 'react';
-import { FormLabel } from 'app/components/FormLabel';
-import { Radio } from 'app/components/Radio';
-import styled from 'styled-components/macro';
-import { useTranslation } from 'react-i18next';
-import { messages } from './messages';
+import * as React from "react";
+import { FormLabel } from "app/components/FormLabel";
+import { Radio } from "app/components/Radio";
+import styled from "styled-components/macro";
+import { useTranslation } from "react-i18next";
+import { messages } from "./messages";
 
 export function LanguageSwitch() {
-  const { t, i18n } = useTranslation();
-  const handleLanguageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const language = event.target.value;
-    i18n.changeLanguage(language);
+  const [gov, setGov] = React.useState(1);
+  // const { t, i18n } = useTranslation();
+  const handleLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const location = Number(event.target.value);
+    console.log({ location: location });
+    setGov(location);
+    // i18n.changeLanguage(language);
   };
 
   return (
     <Wrapper>
+
       <Languages>
         <Radio
           id="3s"
@@ -32,6 +36,27 @@ export function LanguageSwitch() {
           onChange={handleLanguageChange}
           value="de"
           isSelected={i18n.language === 'nb'}
+
+      {/* <FormLabel>{t(...messages.selectLanguage())}</FormLabel> */}
+      <Languages>
+        <Radio
+          id="1"
+          label="أريانة"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="1"
+          isSelected={gov === 1}
+        />
+        <Radio
+          id="2"
+          label="باجة"
+          className="radio"
+          name="language"
+          onChange={handleLocationChange}
+          value="2"
+          isSelected={gov === 2}
+
         />
         <Radio
           id="tr"
